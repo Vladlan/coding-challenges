e8410d693435144a7962b8f9e5292a498c459fe1
Vladislav Lanskikh <vladislav_lanskikh@epam.com>
2023-06-01 16:10:48 +0200

    Add findNextPosition

--- bloxorz-solver/index.js
+++ bloxorz-solver/index.js
@@ -2,51 +2,91 @@
 // |
 // y
 class Box2d {
-    oz = 2;
-    oy = 1;
-    ox = 1;
-    R() {
-        const ox = this.ox;
-        this.ox = this.oz;
-        this.oz = ox;
-        return [this.oy, 1];
+  oz = 2;
+  oy = 1;
+  ox = 1;
+  R(info = false) {
+    if (info) {
+      return [this.oz, 1];
     }
-    U() {
-        const oy = this.oy;
-        this.oy = this.oz;
-        this.oz = oy;
-        return [this.oy, -1];
+    const ox = this.ox;
+    this.ox = this.oz;
+    this.oz = ox;
+    return [this.ox, 1];
+  }
+  U(info = false) {
+    if (info) {
+      return [this.oz, -1];
     }
-    D () {
-        const oy = this.oy;
-        this.oy = this.oz;
-        this.oz = oy;
-        return [this.oy, 1];
+    const oy = this.oy;
+    this.oy = this.oz;
+    this.oz = oy;
+    return [this.oy, 1];
+  }
+  D(info = false) {
+    if (info) {
+      return [this.oz, 1];
     }
-    L () {
-        const ox = this.ox;
-        this.ox = this.oz;
-        this.oz = ox;
-        return [this.ox, -1];
+    const oy = this.oy;
+    this.oy = this.oz;
+    this.oz = oy;
+    return [this.oy, 1];
+  }
+  L(info = false) {
+    if (info) {
+      return [this.oz, -1];
     }
+    const ox = this.ox;
+    this.ox = this.oz;
+    this.oz = ox;
+    return [this.ox, -1];
+  }
 }
 
 getStartPosition = (arr) => {
-    for (let y = 0; y < arr.length; y++) {
-        const row = arr[y];
-        for (let x = 0; x < row.length; x++) {
-            const cell = row[x];
-            if (cell === "B") {
-                return [x, y];
-            }
-        }
+  const bCoords = [];
+  for (let y = 0; y < arr.length; y++) {
+    const row = arr[y];
+    for (let x = 0; x < row.length; x++) {
+      const cell = row[x];
+      if (cell === "B") {
+        bCoords.push([x, y]);
+      }
     }
-}
+  }
+  return bCoords;
+};
 
-function bloxSolver(arr){
-    
-}
+const DIRECTIONS = ["R", "U", "D", "L"];
 
+findNextPosition = (arr, bCoords, box) => {
+  for (const h of DIRECTIONS) {
+    const [d, sign] = box[h](true);
+    const D = d * sign;
+    const newCoords = [];
+    debugger;
+    for (const bCoord of bCoords) {
+      const [bx, by] = bCoord;
+      if (["R", "L"].includes(h)) {
+        const nexBx = arr[by]?.[bx + D];
+        if (nexBx && ["1", "X", "B"].includes(nexBx)) {
+          newCoords.push([bx + D, by]);
+          if (d > 1) newCoords.unshift([bx + sign, by]);
+        }
+      }
+      if (newCoords.length === d) return [newCoords, h];
+      if (["U", "D"].includes(h)) {
+        const nexBy = arr[by + D]?.[bx];
+        if (nexBy && ["1", "X", "B"].includes(nexBy)) {
+          newCoords.push([bx, by + D]);
+          if (d > 1) newCoords.unshift([bx, by + sign]);
+        }
+      }
+      if (newCoords.length === d) return [newCoords, h];
+    }
+  }
+};
 
+function bloxSolver(arr) {}
 
-module.exports = { bloxSolver, Box2d, getStartPosition };
+module.exports = { bloxSolver, Box2d, getStartPosition, findNextPosition };
--- bloxorz-solver/index.spec.js
+++ bloxorz-solver/index.spec.js
@@ -1,10 +1,20 @@
 const { Box2d } = require("./index");
-const level1 = ['1110000000',
-              '1B11110000',
-              '1111111110',
-              '0111111111',
-              '0000011X11',
-              '0000001110'];
+const level1 = () => [
+  "1110000000",
+  "1B11110000",
+  "1111111110",
+  "0111111111",
+  "0000011X11",
+  "0000001110",
+];
+const level2 = () => [
+  "1110000000",
+  "11BB110000",
+  "1111111110",
+  "0111111111",
+  "0000011X11",
+  "0000001110",
+];
 
 describe("Box2d", () => {
   let box2d = null;
@@ -27,8 +37,48 @@ describe("Box2d", () => {
 });
 
 describe("getStartPosition", () => {
-  it("should return start position", () => {
-    const res = getStartPosition(level1);
-    expect(res).toEqual([1, 1]);
+  it("should return start position for level1", () => {
+    const res = getStartPosition(level1());
+    expect(res).toEqual([[1, 1]]);
+  });
+  it("should return start position for level2", () => {
+    const res = getStartPosition(level2());
+    expect(res).toEqual([
+      [2, 1],
+      [3, 1],
+    ]);
   });
 });
+
+describe("findNextPosition", () => {
+  it("should find next position for level1", () => {
+    const [res, d] = findNextPosition(
+      level1(),
+      getStartPosition(level1()),
+      new Box2d()
+    );
+    expect(res).toEqual([
+      [2, 1],
+      [3, 1],
+    ]);
+    expect(d).toEqual("R");
+  });
+  it("should find next position for level2", () => {
+    const box = new Box2d();
+    box.R();
+    const [res, d] = findNextPosition(
+      level2(),
+      getStartPosition(level2()),
+      box
+    );
+    expect(res).toEqual([[3, 1]]);
+    expect(d).toEqual("R");
+  });
+});
+
+// describe("moveBox", () => {
+//   it("should move box", () => {
+//     const res = getStartPosition(level1);
+//     expect(res).toEqual([1, 1]);
+//   });
+// });
